08353a6ebbd9dc98cf2def98f7e8df9959d24b3b
Glen Barnett <gbarnett@jabbyapps.com>
2018-05-01 13:39:22 -0400

    jack ass

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -26,6 +26,7 @@ console.log(ageGlen);
 */
 ////////////////////////////////////////////////////////////////////
 // Lecture: if/else statements
+/*
 var name = 'John';
 var age = 92;
 var isMarried = 'yes';
@@ -42,4 +43,35 @@ if ( isMarried ){
     console.log('Yes');
 } else {
     console.log('No');
+}
+*/
+/////////////////////////////////////////////////////////////////////
+// Lecture: Boolean logic and Switch
+//
+var age = 15;
+
+if ( age < 20) {
+    console.log('John is a teenager');
+} else if (age >= 20 && age < 30) {
+    console.log('John is a young man');
+} else {
+    console.log('John is a man.');
+}
+
+var job = 'teacher';
+
+job = prompt('What does john do? ');
+
+switch (job) {
+    case 'teacher':
+        console.log('John teaches kids.');
+        break;
+    case 'driver':
+        console.log('John drives a cab');
+        break;
+    case 'cop':
+        console.log('John is a copper');
+        break;
+    default:
+        console.log('John does something else');
 }
