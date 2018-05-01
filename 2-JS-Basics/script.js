72cffb5cfded723fe2f3e42d25c5842a23fa940a
Glen Barnett <gbarnett@jabbyapps.com>
2018-05-01 12:04:29 -0400

    update

---
+++ 2-JS-Basics/script.js
@@ -0,0 +1,45 @@
+/*
+var name = 'Erebon';
+console.log(name);
+
+var lastName = 'Inbrain';
+console.log(lastName);
+
+var age = 42;
+console.log('I am ' + age);
+
+var fullAge = true;
+console.log(fullAge);
+
+
+newName = prompt('What is the last name? ');
+console.log('The last name is ' + newName);
+
+alert('Later gator');
+*/
+/*var birthYear = 2018 - 46;
+console.log(birthYear);
+
+var ageGlen = 2018 - 1972;
+ageGlen++;
+console.log(ageGlen);
+*/
+////////////////////////////////////////////////////////////////////
+// Lecture: if/else statements
+var name = 'John';
+var age = 92;
+var isMarried = 'yes';
+
+if (isMarried === 'yes') {
+    console.log(name + ' is married!');
+} else {
+    console.log(name + ' is not married.');
+}
+
+isMarried = true;
+
+if ( isMarried ){
+    console.log('Yes');
+} else {
+    console.log('No');
+}
